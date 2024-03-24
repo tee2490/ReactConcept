6801bb50daf3b71997bbc93fd987a0795b0efa90
tee2490@hotmail.com <tee2490@hotmail.com>
2024-03-24 11:33:28 +0700

    app01-state : Array State

--- app01-state/src/App.jsx
+++ app01-state/src/App.jsx
@@ -1,23 +1,35 @@
 import React, { useState } from "react";
-import './App.css'
+import "./App.css";
 
 export default function App() {
-  const [count, setCount] = useState(0);
+  const [student, setStudent] = useState([
+    { id: 1, name: "Student1" },
+    { id: 2, name: "Student2" },
+    { id: 3, name: "Student3" },
+  ]);
+
+  function deleteStudent(id) {
+    setStudent(student.filter((item) => item.id != id));
+  }
 
   return (
     <div>
-      <h2>{count}</h2>
-      <div>
-        <button onClick={()=>setCount(count+1)} type="button" className="btn btn-primary m-1">
-          Add
-        </button>
-        <button onClick={()=>setCount(0)} type="button" className="btn btn-primary m-1">
-          Reset
-        </button>
-        <button onClick={()=>setCount(count-1)} type="button" className="btn btn-primary m-1">
-          Subtract
-        </button>
-      </div>
+      <h3>All students : {student.length}</h3>
+      <ul>
+        {student.map((item) => (
+          <li key={item.id}>
+            <p>
+              {item.id} {item.name}{" "}
+              <button
+                onClick={() => deleteStudent(item.id)}
+                className="btn btn-success"
+              >
+                Delete
+              </button>
+            </p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
