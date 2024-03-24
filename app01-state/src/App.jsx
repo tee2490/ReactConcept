e76d1fd3a10ee98a81245b90533c397bed38b857
tee2490@hotmail.com <tee2490@hotmail.com>
2024-03-24 11:00:52 +0700

    app01-state : Counter

--- app01-state/src/App.jsx
+++ app01-state/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from "react";
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div>
+      <h2>{count}</h2>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={()=>setCount(count+1)} type="button" className="btn btn-primary m-1">
+          Add
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button onClick={()=>setCount(0)} type="button" className="btn btn-primary m-1">
+          Reset
+        </button>
+        <button onClick={()=>setCount(count-1)} type="button" className="btn btn-primary m-1">
+          Subtract
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
-
-export default App
