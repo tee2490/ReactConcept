e64960736585d9651c547a470d000e287b9cd580
tee2490@hotmail.com <tee2490@hotmail.com>
2024-03-25 15:31:50 +0700

    app01-state : onSubmit Event

--- app01-state/src/components/AddForm.jsx
+++ app01-state/src/components/AddForm.jsx
@@ -3,9 +3,23 @@ import "./AddForm.css";
 
 export default function AddForm() {
   const [name, setName] = useState("");
+
+  function saveStudent(e) {
+    e.preventDefault();
+    if (!name) {
+      alert("กรุณาป้อนข้อมูลด้วยครับ");
+    } else {
+      const newStudent = {
+        id: Math.floor(Math.random() * 1000),
+        name: name,
+      };
+      console.log(newStudent);
+    }
+  }
+
   return (
     <section className="container">
-      <form>
+      <form onSubmit={saveStudent}>
         <label>name</label>
         <input
           type="text"
