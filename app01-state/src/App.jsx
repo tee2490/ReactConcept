8151563ec234c6b23018a637745e50bed8bfe32f
tee2490@hotmail.com <tee2490@hotmail.com>
2024-03-25 13:21:59 +0700

    app01-state : Props from App to StudentList

--- app01-state/src/App.jsx
+++ app01-state/src/App.jsx
@@ -4,11 +4,21 @@ import Studentlist from "./components/Studentlist";
 import './App.css'
 
 export default function App() {
+  const [student, setStudent] = useState([
+    { id: 1, name: "Student1" },
+    { id: 2, name: "Student2" },
+    { id: 3, name: "Student3" },
+  ]);
+
+  function deleteStudent(id) {
+    setStudent(student.filter((item) => item.id != id));
+  }
+
   return (
     <div className="container">
       <Header title="Student list" />
       <main>
-        <Studentlist />
+        <Studentlist student={student} deleteStudent={deleteStudent} />
       </main>
      </div>
   );
--- app01-state/src/components/Studentlist.jsx
+++ app01-state/src/components/Studentlist.jsx
@@ -2,26 +2,15 @@ import React, { useState } from "react";
 import "./Studentlist.css";
 import Item from "./Item";
 
-export default function Studentlist() {
-  const [student, setStudent] = useState([
-    { id: 1, name: "Student1" },
-    { id: 2, name: "Student2" },
-    { id: 3, name: "Student3" },
-  ]);
-
+export default function Studentlist(props) {
+  const { student, deleteStudent } = props;
   const [show, setShow] = useState(true);
-
-  function deleteStudent(id) {
-    setStudent(student.filter((item) => item.id != id));
-  }
 
   return (
     <div>
       <ul>
         <div className="header">
-          <h3>
-            All students : {student.length}
-          </h3>
+          <h3>All students : {student.length}</h3>
           <button onClick={() => setShow(!show)}>
             {show ? "ซ่อน" : "แสดง"}
           </button>
