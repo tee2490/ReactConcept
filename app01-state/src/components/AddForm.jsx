2e6914778ac67d345f5647ddf0185b7ecc04dab5
tee2490@hotmail.com <tee2490@hotmail.com>
2024-03-25 15:14:37 +0700

    app01-state : onChange Event

--- app01-state/src/components/AddForm.jsx
+++ app01-state/src/components/AddForm.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AddForm.css";
 
 export default function AddForm() {
+  const [name, setName] = useState("");
   return (
     <section className="container">
       <form>
         <label>name</label>
-        <input type="text" name="name" />
-        <button type="submit" className="btn-add">save</button>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button type="submit" className="btn-add">
+          save
+        </button>
       </form>
     </section>
   );
