188a7dfe0ec4b016bb6a177c90812732a25ac3a6
tee2490@hotmail.com <tee2490@hotmail.com>
2024-03-25 16:24:46 +0700

    app01-state : Select

--- app01-state/src/components/AddForm.jsx
+++ app01-state/src/components/AddForm.jsx
@@ -4,6 +4,7 @@ import "./AddForm.css";
 export default function AddForm(props) {
   const { student, setStudent } = props;
   const [name, setName] = useState("");
+  const [gender, setGender] = useState("male");
 
   function saveStudent(e) {
     e.preventDefault();
@@ -13,9 +14,11 @@ export default function AddForm(props) {
       const newStudent = {
         id: Math.floor(Math.random() * 1000),
         name: name,
+        gender: gender,
       };
-      setStudent([...student,newStudent])
-      setName("")
+      setStudent([...student, newStudent]);
+      setName("");
+      setGender("male");
     }
   }
 
@@ -29,6 +32,10 @@ export default function AddForm(props) {
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
+        <select value={gender} onChange={(e) => setGender(e.target.value)}>
+          <option value="male">male</option>
+          <option value="female">female</option>
+        </select>
         <button type="submit" className="btn-add">
           save
         </button>
