11b727226e5e3b7f8e96e4ae98c57e43f3bd334c
tee2490@hotmail.com <tee2490@hotmail.com>
2024-04-05 11:25:37 +0700

    app05-router : NavLink and Link

--- app05-router/src/App.jsx
+++ app05-router/src/App.jsx
@@ -3,19 +3,18 @@ import Home from "./components/Home";
 import ProductList from "./components/ProductList";
 import ProductDetail from "./components/ProductDetail";
 import Contact from "./components/Contact";
-import "./App.css"
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <div>
-      <header>HEADER</header>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="products" element={<ProductList />}></Route>
         <Route path="products/1001" element={<ProductDetail />}></Route>
         <Route path="contact" element={<Contact />}></Route>
       </Routes>
-      <footer>FOOTER</footer>
     </div>
   );
 }
