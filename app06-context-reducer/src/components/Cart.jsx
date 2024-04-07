5e220d66d0f062bf9fe857697795a8a232a3126e
tee2490@hotmail.com <tee2490@hotmail.com>
2024-04-07 15:54:08 +0700

    app06-context-reducer : Show products in Cart

--- app06-context-reducer/src/components/Cart.jsx
+++ app06-context-reducer/src/components/Cart.jsx
@@ -1,9 +1,13 @@
 import { Item } from "./Item";
+import { useCart } from "../context/CartContext";
 
 export const Cart = () => {
+  const { products } = useCart();
   return (
     <div>
-      <Item />
+      {products.map((data) => {
+        return <Item key={data.id} {...data} />;
+      })}
     </div>
   );
 };
--- app06-context-reducer/src/components/Item.jsx
+++ app06-context-reducer/src/components/Item.jsx
@@ -1,5 +1,23 @@
-export const Item = () => {
+import "./Item.css";
+
+export const Item = (props) => {
+  const { id, name, price, image, quantity } = props;
   return (
-    <div>Item</div>
-  )
-}
+    <div className="card">
+      <img src={image} />
+      <div className="product">
+        <p className="name">ชื่อสินค้า : {name} </p>
+        <p className="price">ราคา : {price} บาท </p>
+      </div>
+
+      <div className="quantity">
+        <button>+</button>
+        <input type="text" value={quantity} disabled />
+        <button>-</button>
+      </div>
+
+      <div className="total-price">{quantity * price}</div>
+      <button>ลบสินค้า</button>
+    </div>
+  );
+};
