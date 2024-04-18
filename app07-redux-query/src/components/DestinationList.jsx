34ace3985f09d545294caf00cbb7fce65a5ad9c2
tee2490@hotmail.com <tee2490@hotmail.com>
2024-04-18 14:29:25 +0700

    App07-redux-query : Fetch Records from API

--- app07-redux-query/src/components/DestinationList.jsx
+++ app07-redux-query/src/components/DestinationList.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from "react";
+import { useGetAllDestinationQuery } from "../api/destinationApi";
 
 export const DestinationList = () => {
-  return (
-    <div>DestinationList</div>
-  )
-}
+  const { data, isLoading, isSuccess, isError, error } =
+    useGetAllDestinationQuery();
+
+  let content;
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  } else if (isSuccess) {
+    content = data.map((destination) => {
+      return (
+        <article key={destination.id}>
+          <div className="text-center text-info p-2">
+            <div>
+              {destination.city}, {destination.country} -{" "}
+              {destination.daysNeeded} days
+            </div>
+          </div>
+        </article>
+      );
+    });
+  } else if (isError) {
+    content = <p>{error}</p>;
+  }
+  
+  return <div className="pt-3">{content}</div>;
+};
