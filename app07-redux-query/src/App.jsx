75606e1ce3145ffa71c621ef246f9c725c9f4dca
tee2490@hotmail.com <tee2490@hotmail.com>
2024-04-18 12:49:59 +0700

    App07-redux-query : Setup Destination Components

--- app07-redux-query/src/App.jsx
+++ app07-redux-query/src/App.jsx
@@ -1,9 +1,11 @@
+import { DestinationIndex } from "./components";
 import { Header } from "./layout/Header";
 
 export default function App() {
   return (
-    <div>
+    <div className="m-2">
       <Header />
+      <DestinationIndex/>
     </div>
   );
 }
---
+++ app07-redux-query/src/components/DestinationList.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const DestinationList = () => {
+  return (
+    <div>DestinationList</div>
+  )
+}
